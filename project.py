cb3d2a3ca2532f6d0a047f07fb68b03299370c8e
Pavel Raiskup <praiskup@redhat.com>
2015-09-10 12:02:31 +0200

    project.py: atomic support added
    This is work is based on @hhorak's OpenShift PR:
    openshift/postgresql/pull/59
    
    * project.py (atomic_hostdir): New macro.
    (atomic_docker_pcmd): New macro.
    (atomic_install): New macro placeholder.
    (atomic_uninstall): Likewise.

--- project.py
+++ project.py
@@ -34,6 +34,27 @@ config = {
         'contlib': '$pkgdatadir',
 
         # Shell snippet to be called from Dockerfile
-        'docker_container_build': 'container-build && rm /usr/bin/container-build'
+        'docker_container_build':
+            'container-build && rm /usr/bin/container-build',
+
+        # Directory under which 'atomic CMD' will mount Host's '/' directory
+        # into (privileged) container.
+        'atomic_hostdir': '/host',
+
+        # Basic "privileged" docker command
+        'atomic_docker_pcmd':
+            "docker run -t -i --rm --privileged -u 0:0 "  + \
+            "-v /:$atomic_hostdir --net=host --ipc=host --pid=host " + \
+            "-e HOST=$atomic_hostdir " + \
+            '-e LOGDIR=/var/log/"\\${NAME}" ' + \
+            '-e DATADIR=/var/lib/"\\${NAME}" ' + \
+            '-e CONFDIR=/etc/"\\${NAME}" ' + \
+            '-e IMAGE="\\${IMAGE}" -e NAME="\\${NAME}" ' + \
+            '-e OPT1 -e OPT2 -e OPT3 ' + \
+            '\\${OPT2} \\${IMAGE}',
+
+        # Atomic commands, should be redefined in dependant project.
+        'atomic_install': '',
+        'atomic_uninstall': '',
     }
 }
